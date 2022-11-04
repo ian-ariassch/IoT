ff43cb05a0aeb90d680623760be292f96e6aebe0
ian-ariassch <38139729+ian-ariassch@users.noreply.github.com>
2022-11-04 18:57:49 -0500

    Added comments to Q2

--- Lab3/pregunta2.ino
+++ Lab3/pregunta2.ino
@@ -14,9 +14,13 @@ unsigned long flowMilliLitres;
 unsigned int totalMilliLitres;
 
 const char *ssid = "SENNA"; // Enter your WiFi name
-const char *password = "55555555"; // Enter WiFi password
+const char *password = "-"; // Enter WiFi password
 const char *mqtt_broker = "192.168.1.20"; // IP del Broker
+<<<<<<< HEAD
+const char *topic = "water/militotal";
+=======
 const char *topic = "water/militotal"; 
+>>>>>>> 356e3d374a433ff8b982ae357457e19c4be777a9
 const int mqtt_port = 1883;
 
 WiFiClient espClient;
@@ -61,41 +65,46 @@ void setup()
 {
   Serial.begin(9600);
 
-  connectToWifi();
+  connectToWifi(); //Funciones descritas en la pregunta 1
   setupMQTTBroker();
  
   pinMode(SENSOR, INPUT_PULLUP);
  
-  pulseCount = 0;
+  pulseCount = 0;            //Declaración de variables iniciales
   flowRate = 0.0;
   flowMilliLitres = 0;
   totalMilliLitres = 0;
   previousMillis = 0;
  
-  attachInterrupt(digitalPinToInterrupt(SENSOR), pulseCounter, FALLING);
+  attachInterrupt(digitalPinToInterrupt(SENSOR), pulseCounter, FALLING); //Declaración de cuando ocurre un "Interrupt"
+  //El primer parámetro es el PIN en donde se leerá la corriente. En este caso el input de sensor de flujo de agua.
+  //El segundo parámetro es la función a llamar cuando ocurre un interrupt. En este caso, se busca sumar la cantidad de pulsos eléctricos cada vez que ocurre una interrupción.
+  //El tercer parámetros define cuando ocurrirá una interrupción. En este caso, esta definido como "FALLING" que es cuando el pin va de "HIGH" a "LOW". 
 }
  
 void loop()
 {
   currentMillis = millis();
-  if (currentMillis - previousMillis > interval) 
+  if (currentMillis - previousMillis > interval) //Se revisa si es que ha pasado más de 1 segundo.
   {
     
-    pulse1Sec = pulseCount;
+    pulse1Sec = pulseCount; //Se obtiene cuantos pulsos hubo en este segundo y se resetean.
     pulseCount = 0;
  
-    flowRate = ((1000.0 / (millis() - previousMillis)) * pulse1Sec) / calibrationFactor;
-    previousMillis = millis();
+    flowRate = ((1000.0 / (millis() - previousMillis)) * pulse1Sec) / calibrationFactor; //Se calcula el flujo de agua. Se divide 1000 entre el tiempo que ha pasado desde la última ejecución en caso no se haya
+    //completado en exactamente 1 segundo.
+    //Luego, se multiplica ese valor por la cantidad de pulsos obtenidos en este tiempo. Finalmente, se divide entre el calibrationFactor, el cuál es un valor proporcionado por el fabricante del sensor.
+    previousMillis = millis(); 
  
-    flowMilliLitres = (flowRate / 60) * 1000;
+    flowMilliLitres = (flowRate / 60) * 1000; //Se divide entre 60 para obtener litros/segundo y luego se multiplica por 1000 para pasarlo a mililitros.
  
-    totalMilliLitres += flowMilliLitres;
+    totalMilliLitres += flowMilliLitres; //Por último, se suma el flujo al total de mililitros.
      
     Serial.print("Output Liquid Quantity: ");
     Serial.print(totalMilliLitres);
     Serial.print("mL");
 
-    String message = "Ouput Liquid Quantity: ";
+    String message = "Ouput Liquid Quantity: "; //Construcción del mensaje y cast a char[] para que pueda ser enviado como payload en el publish.
     message += totalMilliLitres;
 
      char charBuf[50];
